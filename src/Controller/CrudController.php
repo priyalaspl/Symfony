1246221a8d6091a2c51cc1c9971d2924ace08f4d
Priyal Ghetiya <priyal@aspl.in>
2022-06-28 15:44:20 +0530

    Insert data into table fetching from form
     - Flash messages , asset , template

--- src/Controller/CrudController.php
+++ src/Controller/CrudController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\DBAL\Connection;
 
@@ -12,15 +13,31 @@ class CrudController extends AbstractController
     #[Route('/crud', name: 'app_crud')]
     public function index(Connection $connection): Response
     {
-        $sql = 'SELECT fname FROM netti_user LIMIT 5';
-        $stmt = $connection->query($sql);
-//         $users = $connection->fetchAllAssociative('SELECT * FROM netti_user LIMIT 5');
-        while (($row = $stmt->fetchAssociative()) !== false) {
-            $data[] = $row['fname'];
-        }
+        return $this->render('crud/index.html.twig');
+    }
 
-        return $this->render('crud/index.html.twig', [
-            'data' => $data,
-        ]);
+    #[Route('/store', name: 'store')]
+    public function store(Request $request,Connection $connection): Response
+    {
+       $fname = $request->request->get('fname');
+       $lname = $request->request->get('lname');
+       $email = $request->request->get('email');
+       $number = $request->request->get('number');
+       $country = $request->request->get('country');
+
+       $sql = "INSERT INTO `practice_user` (`fname`, `lname`, `email`, `number`, `country`) VALUES
+                (:fname, :lname, :email, :number, :country)";
+
+        $stmt = $connection->prepare($sql);
+
+        $resultSet = $stmt->executeQuery(['fname' => $fname,'lname' => $lname,'email' => $email,'number' => $number,'country' => $country]);
+
+        // add meessage after insert into user table
+        $this->addFlash(
+            'success',
+            'Data inserted succesfully'
+        );
+
+        return $this->redirectToRoute('app_crud');
     }
 }
